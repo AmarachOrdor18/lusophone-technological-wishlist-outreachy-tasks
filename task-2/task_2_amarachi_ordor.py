f3ec1679ad56bbe5711c1c0b9eb430b6097e6c5b
Amarachi Ordor <128625302+AmarachOrdor18@users.noreply.github.com>
2026-04-15 13:40:34 +0100

    Change GET request to HEAD for URL checking

--- task-2/task_2_amarachi_ordor.py
+++ task-2/task_2_amarachi_ordor.py
@@ -2,12 +2,12 @@ import csv
 import requests
 
 # Open the CSV file and read all URLs
-with open("Task 2 - Intern.csv", newline="", encoding="utf-8-sig") as csvfile:
+with open("Task 2 - Intern.csv", newline="", encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         url = row["urls"].strip()
         try:
-            response = requests.get(url, timeout=10, allow_redirects=True)
+            response = requests.head(url, timeout=10, allow_redirects=True)
             print(f"({response.status_code}) {url}")
         except requests.exceptions.ConnectionError:
             print(f"(CONNECTION ERROR) {url}")
